afe50b93d080973536ab748d117c06b62019cfe4
Bence Fulop <fbence90@gmail.com>
2020-10-30 17:15:49 +1100

    refactored function

--- ruby/resistor-color/resistor_color.rb
+++ ruby/resistor-color/resistor_color.rb
@@ -1,18 +1,7 @@
 class ResistorColor
   COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
-  
+
   def self.color_code(color)
-    case color
-    when "black" then 0
-    when "brown" then 1
-    when "red" then 2
-    when "orange" then 3
-    when "yellow" then 4
-    when "green" then 5
-    when "blue" then 6
-    when "violet" then 7
-    when "grey" then 8
-    when "white" then 9
-    end
+    COLORS.index(color)
   end
 end
