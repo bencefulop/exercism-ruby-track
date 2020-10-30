4cb6c46deee69188f42df724ceea870a5ea35364
Bence Fulop <fbence90@gmail.com>
2020-10-31 08:54:54 +1100

    fix style errors

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -2,8 +2,8 @@ class ResistorColorDuo
 COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
 
   def self.value(colors)
-    output = ""
-    colors[0..1].each { |color| output += "#{COLORS.index(color)}" }
-    output.to_i
+    color_code = ""
+    colors[0..1].each { |color| color_code += "#{COLORS.index(color)}" }
+    color_code.to_i
   end
 end
