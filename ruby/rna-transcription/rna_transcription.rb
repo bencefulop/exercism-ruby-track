752dc470a2e6bf15ff56d1a7c098a72b0a32970f
Bence Fulop <fbence90@gmail.com>
2020-11-02 11:53:53 +1100

    All tests pass, method refactored

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -9,8 +9,10 @@ A: "U"
   def self.of_dna(nucleotides)
     rna_output = ""
     nucleotides
-      .split
-      .each { |n| rna_output += COMPLEMENTS[:n] }
+      .split("")
+      .each do |n| 
+        COMPLEMENTS[:"#{n}"] ? rna_output += COMPLEMENTS[:"#{n}"] : return
+      end
     rna_output
   end
 end
