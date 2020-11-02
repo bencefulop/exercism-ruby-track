111180cf7e5e4bed1db3f9bcf8243e3903cf85d0
Bence Fulop <fbence90@gmail.com>
2020-11-02 18:53:19 +1100

    change case statement, refactoring in process

--- ruby/microwave/microwave.rb
+++ ruby/microwave/microwave.rb
@@ -1,31 +1,37 @@
-# require '/pry'
+require 'pry'
 
 class Microwave
+  attr_reader :time
+  
   def initialize(time)
     @time = time
   end
   
   def timer
-    # binding.pry
-    case @time
-    when 0...10 
-      "00:0#{@time}"
-    when 10...60
-      "00:#{@time}"
-    when 60, 120
-      "0#{@time / 60}:0#{@time % 60}"
-    when 61...100
-      @time % 60 > 9 ? "0#{@time / 60}:#{@time % 60}" : "0#{@time / 60}:0#{@time % 60}"       
-    when 100, 200
-      "0#{@time / 100}:00"
-    when 101...110
-      "0#{@time / 100}:0#{@time - 100}"
-    when 110...160
-      "0#{@time / 100}:#{@time - 100}"
-    when 1001
-      "10:01"
-    when 272
-      "03:12"
-    end
+    string_time = @time.to_s
   end
+
+
+  # def timer
+  #   seconds = @time
+  #   # binding.pry
+  #   case
+  #   when seconds < 60
+  #     seconds < 10 ? "00:0#{seconds}" : "00:#{seconds}"
+  #   when seconds / 60 === 0
+  #     "0#{seconds / 60}:0#{seconds % 60}"
+  #   when seconds < 100
+  #     seconds % 60 > 9 ? "0#{seconds / 60}:#{seconds % 60}" : "0#{seconds / 60}:0#{seconds % 60}"       
+  #   when seconds == 100, seconds == 200
+  #     "0#{seconds / 100}:00"
+  #   when seconds < 110
+  #     "0#{seconds / 100}:0#{seconds - 100}"
+  #   when seconds < 160
+  #     "0#{seconds / 100}:#{seconds - 100}"
+  #   when seconds == 1001
+  #     "10:01"
+  #   when seconds == 272
+  #     "03:12"
+  #   end
+  # end
 end
