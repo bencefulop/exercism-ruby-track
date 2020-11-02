2bf59193ad50c4bd52603f6f7cb3c6b2e2475887
Bence Fulop <fbence90@gmail.com>
2020-11-03 10:57:42 +1100

    function pass all the tests, more readable

--- ruby/microwave/microwave.rb
+++ ruby/microwave/microwave.rb
@@ -6,65 +6,26 @@ class Microwave
 
   def timer
     seconds = @time.to_s
+    
     case seconds.length
     when 1 then "00:0#{seconds}"
     when 2
-      if @time % 60 == 0
-        convert_to_minutes
+      if @time == 60
+        "01:00"
       elsif @time < 60
         "00:#{seconds}"
       else
         @time % 60 > 9 ? "0#{@time / 60}:#{@time % 60}" : "0#{@time / 60}:0#{@time % 60}"
       end
     when 3
-      if @time % 60 == 0
-        convert_to_minutes
-      else seconds[1].to_i > 5
-      # else 
-
+      if seconds[1].to_i < 6
+        "0#{seconds[0]}:#{seconds[1..2]}"
+      else 
+        "0#{(@time + 100).to_s[0]}:#{(@time - 60).to_s[1..2]}"
       end
     when 4
-      if @time % 60 == 0
-      convert_to_minutes
-      else
         seconds[1] += ":"
         seconds
-      end
     end
   end
-
-  def convert_to_minutes
-    if @time / 60 < 10
-      "0#{@time / 60}:00"
-    else
-      "#{@time / 60}:00"
-    end
-  end
-
-
-
-
-  # def timer
-  #   seconds = @time
-  #   case
-  #   when seconds < 60
-  #     seconds < 10 ? "00:0#{seconds}" : "00:#{seconds}"
-  #   when seconds / 60 == 0
-  #     "0#{seconds / 60}:0#{seconds % 60}"
-  #   when seconds < 100
-  #     seconds % 60 > 9 ? "0#{seconds / 60}:#{seconds % 60}" : "0#{seconds / 60}:0#{seconds % 60}"       
-  #   when seconds == 100, seconds == 200
-  #     "0#{seconds / 100}:00"
-  #   when seconds < 110
-  #     "0#{seconds / 100}:0#{seconds - 100}"
-  #   when seconds < 160
-  #     "0#{seconds / 100}:#{seconds - 100}"
-  #   when (seconds.to_s).length == 4
-  #     string_seconds = (seconds.to_s)
-  #     string_seconds[1] += ":"
-  #     string_seconds
-  #   when seconds == 272
-  #     "03:12"
-  #   end
-  # end
 end
