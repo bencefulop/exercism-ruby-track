0ca9d794676b1647343cc26579c49bc8b1d41054
Bence Fulop <fbence90@gmail.com>
2020-11-09 12:18:03 +1100

    refactor resistor duo && download space age

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,9 +1,9 @@
 class ResistorColorDuo
-COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+  COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
 
   def self.value(colors)
     color_code = ""
-    colors[0..1].each { |color| color_code += "#{COLORS.index(color)}" }
+    colors[0..1].each { |color| color_code += COLORS.index(color).to_s }
     color_code.to_i
   end
 end
