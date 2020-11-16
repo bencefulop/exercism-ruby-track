3c263d8fcf1d28b4fe81ff9a0413110e8e4fac2c
Bence Fulop <fbence90@gmail.com>
2020-11-16 14:11:30 +1100

    download ISBN exercise

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,8 +1,2 @@
-=begin
-Write your code for the 'Space Age' exercise in this file. Make the tests in
-`space_age_test.rb` pass.
-
-To get started with TDD, see the `README.md` file in your
-`ruby/space-age` directory.
-=end
-
+class SpaceAge
+end
