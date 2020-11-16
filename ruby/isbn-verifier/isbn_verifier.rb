a8fb0a3311fd58ad6c73cdb52b069050d5b4adbc
Bence Fulop <fbence90@gmail.com>
2020-11-16 17:53:38 +1100

    solved exercise

--- ruby/isbn-verifier/isbn_verifier.rb
+++ ruby/isbn-verifier/isbn_verifier.rb
@@ -1,8 +1,16 @@
-=begin
-Write your code for the 'ISBN Verifier' exercise in this file. Make the tests in
-`isbn_verifier_test.rb` pass.
+require 'pry'
 
-To get started with TDD, see the `README.md` file in your
-`ruby/isbn-verifier` directory.
-=end
+class IsbnVerifier
+  def self.valid?(number)
+    number.gsub!("-", "")
+    return false unless number =~ /^\d{9}[\d|X]$/    
 
+    sum = 0
+
+    number.split("").each_with_index do |e, i|
+        e = "10" if e == "X"
+        sum += (e.to_i * (10 - i))
+      end
+    sum % 11 == 0
+  end
+end
