3f1c9f921260ff84134fdb0842e221f7db363258
Bence Fulop <fbence90@gmail.com>
2020-11-18 17:59:48 +1100

    on earth method working, mercury return too big of a difference

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,2 +1,13 @@
 class SpaceAge
+  def initialize(seconds)
+    @seconds = seconds
+  end
+
+  def on_earth
+    ((((@seconds.to_f / 60).floor(2) / 60).floor(2) / 24).floor(2) / 365).floor(2)
+  end
+
+  def on_mercury
+    (on_earth / 0.2408467).floor(2)
+  end
 end
