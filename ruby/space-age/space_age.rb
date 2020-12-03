4864ea4f6c33b51de7262e4e31b68802542ee493
Bence Fulop <fbence90@gmail.com>
2020-12-03 13:20:43 +1100

    All tests pass

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -20,4 +20,24 @@ class SpaceAge
   def on_mars
     (on_earth / 1.8808158).floor(2)
   end
+
+  def on_jupiter
+    # (on_earth / 11.862615).floor(3)
+    (on_earth / 11.862615).round(2)
+    # either of these work
+  end
+
+  def on_saturn
+    (on_earth / 29.447498).floor(2)
+    # (on_earth / 29.447498).round(2)
+  end
+
+  def on_uranus
+    (on_earth / 84.016846).floor(2)
+  end
+
+  def on_neptune
+    # (on_earth / 164.79132).floor(2)
+    (on_earth / 164.79132).round(2)
+  end
 end
