8f61b3b35cb4bf211164a92f991c20a92fccb74f
Bence Fulop <fbence90@gmail.com>
2021-01-03 16:10:06 +1100

    syntax fixes for final solution

--- ruby/isbn-verifier/isbn_verifier.rb
+++ ruby/isbn-verifier/isbn_verifier.rb
@@ -1,13 +1,11 @@
-require 'pry'
-
 class IsbnVerifier
   def self.valid?(number)
     number.gsub!("-", "")
     return false unless number =~ /^\d{9}[\d|X]$/
 
-    number.split("").each_with_index.map do |e, i|
+    number.chars().each_with_index.map do |e, i|
         e = "10" if e == "X"
-       (e.to_i * (10 - i))
+       e.to_i * (10 - i)
     end.sum % 11 == 0
   end
 end
