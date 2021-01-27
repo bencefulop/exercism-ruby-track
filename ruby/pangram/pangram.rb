db94089c3b6165f359f3c5f758c97ad9f24044c6
Bence Fulop <fbence90@gmail.com>
2021-01-27 21:51:49 +1100

    All test pass with downcase called in

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -1,6 +1,7 @@
 class Pangram
-  LETTERS = %w[a b c d e f g h i j k l m n o p q r s t u v w x y z]
+  LETTERS = ("a".."z").to_a
+  
   def self.pangram?(sentence)
-    (LETTERS - (sentence.chars)).empty?
+    (LETTERS - (sentence.downcase.chars)).empty?
   end
 end
