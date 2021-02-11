f85b6089bd707bb6cc29c792489c1b67a251bf7a
Bence Fulop <fbence90@gmail.com>
2021-02-11 18:11:49 +1100

    refactoring for better readability
    - added sides_are_invalid? method to check if any of the sides equal to zero
    - changed isosceles? if statement to case statement

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -10,27 +10,28 @@ class Triangle
 
   def equilateral?
     return false if not_valid_triangle?
-    return false if @a == 0
+    return false if sides_are_invalid?
+
     return true if @a == @b && @b == @c
   end
   
   def isosceles?
     return false if not_valid_triangle?
-
-    if @a == @b && @b != @c 
-      return true
-    elsif @a == @b && @b == @c
-      return true
-    elsif @a == @c && @c != @b
-      return true
-    elsif @b == @c && @c != @a
-      return true
+    return false if sides_are_invalid?
+    
+    case 
+    when @a == @b && @b != @c then true
+    when @a == @b && @b == @c then true
+    when @a == @c && @c != @b then true
+    when @b == @c && @c != @a then true
     end
-
+    
   end
-
+  
   def scalene?
     return false if not_valid_triangle?
+    return false if sides_are_invalid?
+
     return true if @a != @b && @b != @c && @c != @a
   end
 
@@ -38,5 +39,9 @@ class Triangle
 
   def not_valid_triangle?
     @a + @b < @c || @a + @c < @b || @b + @c < @a
+  end
+
+  def sides_are_invalid?
+    @a.zero? || @b.zero? || @c.zero?
   end
 end
