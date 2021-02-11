700bd78a4d188a3068d7bf0be9fe60445c2b5e0e
Bence Fulop <fbence90@gmail.com>
2021-02-11 18:31:53 +1100

    rubocop check

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -1,11 +1,9 @@
 class Triangle
-
   def initialize(sides)
     @sides = sides
     @a = @sides[0]
     @b = @sides[1]
     @c = @sides[2]
-    
   end
 
   def equilateral?
@@ -14,20 +12,18 @@ class Triangle
 
     return true if @a == @b && @b == @c
   end
-  
+
   def isosceles?
     return false if not_valid_triangle?
     return false if sides_are_invalid?
-    
-    case 
-    when @a == @b && @b != @c then true
-    when @a == @b && @b == @c then true
-    when @a == @c && @c != @b then true
-    when @b == @c && @c != @a then true
+
+    if @a == @b && @b != @c then true
+    elsif @a == @b && @b == @c then true
+    elsif @a == @c && @c != @b then true
+    elsif @b == @c && @c != @a then true
     end
-    
   end
-  
+
   def scalene?
     return false if not_valid_triangle?
     return false if sides_are_invalid?
