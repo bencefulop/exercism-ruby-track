caf52d23de47ba62b3c81ec44d25a4122dff5ce7
Bence Fulop <fbence90@gmail.com>
2021-02-11 18:04:38 +1100

    move triengle validity check to private method

--- ruby/triangle/triangle.rb
+++ ruby/triangle/triangle.rb
@@ -9,14 +9,13 @@ class Triangle
   end
 
   def equilateral?
+    return false if not_valid_triangle?
     return false if @a == 0
-
     return true if @a == @b && @b == @c
   end
   
   def isosceles?
-    
-  return false if @a + @b < @c || @a + @c < @b || @b + @c < @a
+    return false if not_valid_triangle?
 
     if @a == @b && @b != @c 
       return true
@@ -31,9 +30,13 @@ class Triangle
   end
 
   def scalene?
-    return false if @a + @b < @c || @a + @c < @b || @b + @c < @a
-
-
+    return false if not_valid_triangle?
     return true if @a != @b && @b != @c && @c != @a
+  end
+
+  private
+
+  def not_valid_triangle?
+    @a + @b < @c || @a + @c < @b || @b + @c < @a
   end
 end
