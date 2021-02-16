c5606732823b45675ba12097ffe6e4949033cdc4
Bence Fulop <fbence90@gmail.com>
2021-02-16 21:54:30 +1100

    function to convert numbers to rows in progress

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,8 +1,19 @@
-=begin
-Write your code for the 'Matrix' exercise in this file. Make the tests in
-`matrix_test.rb` pass.
+require 'pry'
 
-To get started with TDD, see the `README.md` file in your
-`ruby/matrix` directory.
-=end
+class Matrix
 
+  attr_reader :rows
+
+  def initialize(numbers)
+    @numbers = numbers.split
+    @rows = extract_rows(@numbers)
+    # binding.pry
+    # // TODO // 
+    # need to save @rows and @colums. need attr_readers too
+  end
+
+  def extract_rows(numbers)
+    array = number.split.map {|n| n.to_i }
+    
+  end
+end
