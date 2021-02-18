575bb1c851add355d1f5d927692c434290c2874f
Bence Fulop <fbence90@gmail.com>
2021-02-18 22:26:03 +1100

    columns method in progress

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -2,17 +2,30 @@ require 'pry'
 
 class Matrix
 
-  attr_reader :rows
+  attr_reader :rows, :columns
 
   def initialize(numbers)
-    @numbers = numbers
+    @numbers = numbers.split("\n")
     @rows = extract_rows(@numbers)
+    @columns = extract_columns(@numbers)
   end
 
   def extract_rows(numbers)
-    elements = numbers.split("\n")
-    result = elements.map{ |n| n.split("\s") }
-    result.map {|e| e.map {|n| n.to_i}}
-    
+    numbers.map { |n| n.split("\s") }.map { |e| e.map { |n| n.to_i } }
+  end
+
+  def extract_columns(numbers)
+    # @rows.map do |row|
+    #   row.map.with_index do |n,i|
+        
+    #   end
+    # end
+    counter = 0
+    result = @rows.map.with_index do |n,i| 
+      # binding.pry
+      n[counter]
+      counter += 1
+    end
+    result
   end
 end
