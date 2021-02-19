debc1869dd28992a7f2ed3475814c905f0294ee8
Bence Fulop <fbence90@gmail.com>
2021-02-19 15:00:35 +1100

    all tests passing

--- ruby/matrix/matrix_test.rb
+++ ruby/matrix/matrix_test.rb
@@ -32,13 +32,13 @@ class MatrixTest < Minitest::Test
   end
 
   def test_extract_another_column
-    skip
+    # skip
     matrix = Matrix.new("89 1903 3\n18 3 1\n9 4 800")
     assert_equal [1903, 3, 4], matrix.columns[1]
   end
 
   def test_extract_last_column
-    skip
+    # skip
     matrix = Matrix.new("1 2 3\n10 20 30\n100 200 300")
     assert_equal [3, 30, 300], matrix.columns.last
   end
