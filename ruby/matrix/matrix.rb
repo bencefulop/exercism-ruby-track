34c8418c22225ce6fb318d33f328d40c98b236d1
Bence Fulop <fbence90@gmail.com>
2021-04-29 10:08:19 +1000

    refactor based on mentor feedback

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,24 +1,14 @@
 class Matrix
-  attr_reader :rows, :columns
 
   def initialize(numbers)
-    @numbers = numbers.split("\n")
-    @rows = extract_rows(@numbers)
-    @columns = extract_columns(@numbers)
+    @numbers = numbers
   end
 
-  def extract_rows(numbers)
-    numbers.map { |n| n.split("\s") }.map { |e| e.map(&:to_i) }
+  def rows
+    rows ||= @numbers.split("\n").map(&:split).map { |e| e.map(&:to_i) }
   end
 
-  def extract_columns(_numbers)
-    columns_array = Array.new(@rows.first.length) { [] }
-
-    @rows.each do |row|
-      row.each_with_index do |num, i|
-        columns_array[i] << num
-      end
-    end
-    columns_array
+  def columns
+    rows.transpose
   end
 end
