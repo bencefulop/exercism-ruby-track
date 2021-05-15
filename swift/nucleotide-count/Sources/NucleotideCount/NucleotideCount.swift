f54dc8df0cf10d050c9ae25ea0243c1a5548fadd
Bence Fulop <fbence90@gmail.com>
2021-05-15 14:20:36 +1000

    all but one tests passing

--- swift/nucleotide-count/Sources/NucleotideCount/NucleotideCount.swift
+++ swift/nucleotide-count/Sources/NucleotideCount/NucleotideCount.swift
@@ -1,1 +1,24 @@
 //Solution goes in Sources
+//Solution goes in Sources
+class DNA {
+    let strand: String
+
+    init?(strand: String) {
+        self.strand = strand
+    }
+
+    func count(_ myChar: Character) -> Int {
+        self.strand.filter { $0 == myChar }.count
+    }
+
+    func counts() -> [String : Int] {
+        let thymineNumber: Int = count("T")
+        let adenineNumber: Int = count("A")
+        let cytosineNumber: Int = count("C")
+        let guanineNumber: Int = count("G")
+
+        return ["T": thymineNumber, "A": adenineNumber, "C": cytosineNumber, "G": guanineNumber]
+    }
+
+}
+
