d61df1207a020bc9fb8631be1a6c4ac8c42c649c
Bence Fulop <fbence90@gmail.com>
2021-06-03 21:28:26 +1000

    another leap iteration

--- swift/leap/Sources/Leap/Leap.swift
+++ swift/leap/Sources/Leap/Leap.swift
@@ -1,7 +1,7 @@
 //Solution goes in Sources
 struct Year {
-    var isLeapYear: Bool = false
-    
+    let isLeapYear: Bool
+
     init(calendarYear: Int) {
         self.isLeapYear = calendarYear.isMultiple(of: 4) && ((calendarYear.isMultiple(of: 400) && calendarYear.isMultiple(of: 100)) || !calendarYear.isMultiple(of: 100))
     }
