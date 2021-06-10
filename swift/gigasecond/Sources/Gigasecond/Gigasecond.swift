47bb7bc659a449388ac9f8ff78debbc0f4dc8de2
Bence Fulop <fbence90@gmail.com>
2021-06-10 21:31:37 +1000

    gigasecond tests pass

--- swift/gigasecond/Sources/Gigasecond/Gigasecond.swift
+++ swift/gigasecond/Sources/Gigasecond/Gigasecond.swift
@@ -1,27 +1,18 @@
 //Solution goes in Sources
 import Foundation
 
-@available(macOS 10.12, *)
 class Gigasecond {
-    
+
     var description: String?
-    
+    let timeInterval: Double = 1_000_000_000
+
     init?(from: String) {
-        let dateFormatter = ISO8601DateFormatter()
-        let timeInterval: Double = 1_000_000_000
-        let date = dateFormatter.date(from: from + "+0000")
-        self.description = "\(Date(timeInterval: timeInterval, since: date!))" + "+0000"
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "en_US_POSIX")
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+        dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
+        let date = dateFormatter.date(from: from)
+        let newDate = Date(timeInterval: timeInterval, since: date!)
+        self.description = dateFormatter.string(from: newDate)
     }
 }
-
-//let dateFormatterGet = DateFormatter()
-//dateFormatterGet.dateFormat = "yyyy-MM-dd HH:mm:ss"
-//
-//let dateFormatterPrint = DateFormatter()
-//dateFormatterPrint.dateFormat = "MMM dd,yyyy"
-//
-//if let date = dateFormatterGet.date(from: "2016-02-29 12:24:26") {
-//    print(dateFormatterPrint.string(from: date))
-//} else {
-//   print("There was an error decoding the string")
-//}
