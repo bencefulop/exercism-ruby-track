d8b934eda0eb8438f5571e2a901cdb043e5d4efa
Bence Fulop <fbence90@gmail.com>
2021-06-12 16:30:25 +1000

    second iteration of gigasecond

--- swift/gigasecond/Sources/Gigasecond/Gigasecond.swift
+++ swift/gigasecond/Sources/Gigasecond/Gigasecond.swift
@@ -1,18 +1,22 @@
 //Solution goes in Sources
 import Foundation
 
-class Gigasecond {
+struct Gigasecond {
 
-    var description: String?
+    let description: String?
     let timeInterval: Double = 1_000_000_000
-
-    init?(from: String) {
+    
+    private static let dateFormatter: DateFormatter = {
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "en_US_POSIX")
         dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
         dateFormatter.timeZone = TimeZone(secondsFromGMT: 0)
-        let date = dateFormatter.date(from: from)
-        let newDate = Date(timeInterval: timeInterval, since: date!)
-        self.description = dateFormatter.string(from: newDate)
+        return dateFormatter
+    }()
+
+    init?(from: String) {
+        guard let date = Gigasecond.dateFormatter.date(from: from) else { return nil }
+        let newDate = Date(timeInterval: timeInterval, since: date)
+        self.description = Gigasecond.dateFormatter.string(from: newDate)
     }
 }
