03d1c20a1ef07c36f69a718b8366e330d94a1d3f
Bence Fulop <bence.fulop@arq.group>
2021-09-26 18:29:32 +1000

    accumulate solution

--- swift/accumulate/Sources/Accumulate/Accumulate.swift
+++ swift/accumulate/Sources/Accumulate/Accumulate.swift
@@ -1,1 +1,11 @@
 //Solution goes in Sources
+
+extension Array {
+    func accumulate<T>(_ function: (Element) -> (T)) -> [T] {
+        var array = [T]()
+        for item in self {
+            array.append(function(item))
+        }
+        return array
+    }
+}
