18c724154b55488e41055cc7506788d092e4ccc6
Bence Fulop <bence.fulop@arq.group>
2021-09-26 19:06:23 +1000

    bob exercise progress

--- swift/bob/Sources/Bob/Bob.swift
+++ swift/bob/Sources/Bob/Bob.swift
@@ -1,1 +1,29 @@
 //Solution goes in Sources
+import Foundation
+
+struct Bob {
+    static func hey(_ input: String) -> String {
+        var answer = ""
+        let shoutedInput = input.uppercased()
+        
+        
+        if input.last == "." {
+            answer = "Whatever."
+        }
+
+        if input.last == "?" {
+            answer = "Sure."
+        }
+        
+        if input == shoutedInput {
+            answer = "Whoa, chill out!"
+        }
+        
+        if input.trimmingCharacters(in: .whitespaces).isEmpty {
+            answer = "Fine. Be that way!"
+        }
+
+
+        return answer
+    }
+}
