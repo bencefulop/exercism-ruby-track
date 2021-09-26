af67bce35ba6354743a79ece8544017e0402238c
Bence Fulop <bence.fulop@arq.group>
2021-09-26 15:23:48 +1000

    Difference of squares second iteration

--- swift/difference-of-squares/Sources/DifferenceOfSquares/DifferenceOfSquares.swift
+++ swift/difference-of-squares/Sources/DifferenceOfSquares/DifferenceOfSquares.swift
@@ -2,43 +2,38 @@
 
 import Foundation
 
-struct Squares {
-    let squareOfSum: Int
-    let sumOfSquares: Int
-    let differenceOfSquares: Int
+class Squares {
+    let n: Int
     
-    init(_ n: Int) {
-        self.squareOfSum = Squares.calculateSquareOfSum(n)
-        self.sumOfSquares = Squares.calculateSumOfSquares(n)
-        self.differenceOfSquares = Squares.calculateDifferenceOfSquares(n)
-    }
-    
-    static func calculateSquareOfSum(_ n: Int) -> Int {
-        var result: Double = 0.0
-        var number = 0
-        
-        while number < n {
-            result += Double(number + 1)
-            number += 1
-        }
-        
-        return Int(pow(result, 2.0))
-    }
-    
-    static func calculateSumOfSquares(_ n: Int) -> Int {
+    lazy var sumOfSquares: Int = {
         var result: Int = 0
         var number = 0
         
-        while number < n {
+        while number < self.n {
             result += Int(pow(Double(number + 1), 2.0))
             number += 1
         }
         
         return result
-    }
+    }()
+
+    lazy var squareOfSum: Int = {
+        var result: Double = 0.0
+        var number = 0
+        
+        while number < self.n {
+            result += Double(number + 1)
+            number += 1
+        }
+        
+        return Int(pow(result, 2.0))
+    }()
     
-    static func calculateDifferenceOfSquares(_ n: Int) -> Int {
-        return calculateSquareOfSum(n) - calculateSumOfSquares(n)
-    }
+    lazy var differenceOfSquares: Int = {
+        self.squareOfSum - self.sumOfSquares
+    }()
     
+    init(_ n: Int) {
+        self.n = n
+    }
 }
