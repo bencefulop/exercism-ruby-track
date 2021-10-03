320fa720bbc758927c7664bbea0285770431fea4
Bence Fulop <bence.fulop@arq.group>
2021-10-04 09:00:40 +1100

    bob all tests pass

--- swift/bob/Sources/Bob/Bob.swift
+++ swift/bob/Sources/Bob/Bob.swift
@@ -6,24 +6,30 @@ struct Bob {
         var answer = ""
         let shoutedInput = input.uppercased()
         
-        
-        if input.last == "." {
+        switch input {
+        case let input where input.trimmingCharacters(in: .whitespaces).isEmpty:
+            answer = "Fine. Be that way!"
+        case let input where input == shoutedInput:
+            if input.digitsOnly && input.last == "?" {
+                answer = "Sure."
+            }
+            else if input.digitsOnly && input.last == "." {
+                answer = "Whatever."
+            } else {
+                answer = "Whoa, chill out!"
+            }
+        case let input where input.last == "?":
+            answer = "Sure."
+        default:
             answer = "Whatever."
         }
-
-        if input.last == "?" {
-            answer = "Sure."
-        }
-        
-        if input == shoutedInput {
-            answer = "Whoa, chill out!"
-        }
-        
-        if input.trimmingCharacters(in: .whitespaces).isEmpty {
-            answer = "Fine. Be that way!"
-        }
-
-
         return answer
+    }
+}
+
+extension String {
+    var digitsOnly: Bool {
+        let digitsOfSting = components(separatedBy: CharacterSet.decimalDigits.inverted).joined()
+        return !(digitsOfSting.isEmpty) && digitsOfSting.allSatisfy { $0.isNumber }
     }
 }
